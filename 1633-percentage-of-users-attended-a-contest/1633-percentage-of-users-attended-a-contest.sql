356aefa65e6af2d29540a8a2f758ab2e8edb5325
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-20 18:46:45 +0900

    Time: 1101 ms (80.68%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,6 @@
+select
+    contest_id,
+    round(count(distinct user_id)/(select count(user_id) from Users)*100, 2) as percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id
