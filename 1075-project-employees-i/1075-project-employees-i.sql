b26155590f72a8f66dd0c0ca5c9b8a14c9269a97
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-20 18:32:41 +0900

    Time: 407 ms (99.85%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,7 @@
+select
+    p.project_id as project_id,
+    round(avg(e.experience_years),2) as average_years
+from Project as p
+left join Employee as e on
+    p.employee_id=e.employee_id
+group by project_id
