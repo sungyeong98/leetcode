6d198d676082fbd00a0cbe48e22a4e44c896504a
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-20 19:06:32 +0900

    Time: 630 ms (83.88%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,9 @@
+select
+    date_format(trans_date,'%Y-%m') as month,
+    country,
+    count(state) as trans_count,
+    sum(if(state='approved',1,0)) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(if(state='approved',amount,0)) as approved_total_amount
+from Transactions
+group by date_format(trans_date,'%Y-%m'), country
