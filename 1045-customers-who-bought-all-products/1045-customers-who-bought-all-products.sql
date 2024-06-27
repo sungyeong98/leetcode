dd310078a6e8d4691e590180c2c3a73ed02a1fcb
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-27 17:10:40 +0900

    Time: 509 ms (90.63%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,4 +1,11 @@
 select customer_id
-from Customer
-group by customer_id
-having sum(distinct product_key)=(select sum(product_key) from Product)
+from (
+    select 
+        case
+            when count(distinct product_key)=(select count(product_key) from Product)
+                then customer_id 
+        end as customer_id
+    from Customer c
+    group by customer_id
+) temp
+where customer_id is not null
