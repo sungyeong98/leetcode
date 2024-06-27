8edd414b102db85d6c11e766936bf1ac08c36f0e
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-27 18:19:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,14 @@
+with temp as (
+    select 
+        client_id, request_at,
+        case when status='completed' then 0 else 1 end as status
+    from Trips
+    where 
+        client_id not in (select users_id from Users where banned='Yes') and
+        request_at between "2013-10-01" and "2013-10-03"
+)
+select
+    request_at as Day,
+    round(sum(status)/count(status),2) as 'Cancellation Rate'
+from temp
+group by request_at
