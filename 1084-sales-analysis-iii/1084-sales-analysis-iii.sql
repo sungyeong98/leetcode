4cfb22452751d8d4e4b9a431ba3af1186ec2dee0
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-28 16:01:20 +0900

    Time: 1483 ms (65.26%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -2,7 +2,13 @@ with not_target as (
     select product_id
     from Sales
     where sale_date<'2019-01-01' or sale_date>'2019-03-31'
+), target as (
+    select product_id
+    from Sales
+    where sale_date between '2019-01-01' and '2019-03-31'
 )
 select product_id, product_name
 from Product
-where product_id not in (select distinct product_id from not_target)
+where 
+    product_id not in (select distinct product_id from not_target) and
+    product_id in (select product_id from target)
