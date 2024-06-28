0168d9a6645b677640db0333c559605a518e144a
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-06-28 18:21:26 +0900

    Time: 711 ms (11.54%), Space: 54.6 MB (5.8%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -2,9 +2,9 @@ from collections import defaultdict
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
         temp=defaultdict(list)
+
         for i,j in edges:
             temp[i].append(j)
             temp[j].append(i)
-        temp_dict=dict(sorted(temp.items(), reverse=True, key=lambda x:len(x[1])))
 
-        return list(temp_dict.keys())[0]
+        return max(temp, key=lambda x:len(temp[x]))
