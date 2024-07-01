8363ed8e0547a2d4524b6161d850ddb3dc4d061e
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-01 17:15:26 +0900

    Time: 88 ms (19.94%), Space: 17 MB (8.25%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,10 +1,31 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if not nums1:
-            return sum(nums2)/len(nums2)
-        if not nums2:
-            return sum(nums1)/len(nums1)
+        m, n = len(nums1), len(nums2)
+        dm = dn = 0
+        temp=[]
         
-        n1=sum(nums1)/len(nums1)
-        n2=sum(nums2)/len(nums2)
-        return (n1+n2)/2
+        while dm<len(nums1) and dn<len(nums2):
+            if nums1[dm]<nums2[dn]:
+                temp.append(nums1[dm])
+                dm+=1
+            elif nums1[dm]>nums2[dn]:
+                temp.append(nums2[dn])
+                dn+=1
+            elif nums1[dm]==nums2[dn]:
+                temp.append(nums1[dm])
+                temp.append(nums2[dn])
+                dm+=1
+                dn+=1
+        
+        while dm<len(nums1):
+            temp.append(nums1[dm])
+            dm+=1
+        while dn<len(nums2):
+            temp.append(nums2[dn])
+            dn+=1
+
+        if (m+n)%2==1:
+            return temp[(m+n)//2]
+        else:
+            mid=(m+n)//2
+            return (temp[mid]+temp[mid-1])/2
