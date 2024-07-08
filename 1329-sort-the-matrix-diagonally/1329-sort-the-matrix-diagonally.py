a3c512bf0d89aa96ed5b86915270e9cff77c9fcc
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-08 18:14:51 +0900

    Time: 76 ms (26.85%), Space: 16.9 MB (50.18%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,5 +1,29 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        temp=[]
+        m,n=len(mat),len(mat[0])
+
+        for i in range(m):
+            temp.append([i,0])
+        for i in range(n):
+            temp.append([0,i])
+        temp.pop(0)
+        
+        for x,y in temp:
+            arr=[]
+            nx,ny=x,y
+
+            while nx<m and ny<n:
+                arr.append(mat[nx][ny])
+                nx,ny=nx+1,ny+1
+            arr.sort()
+            
+            nx,ny=x,y
+            while nx<m and ny<n:
+                mat[nx][ny]=arr.pop(0)
+                nx,ny=nx+1,ny+1
+        return mat
+        '''
         m,n=len(mat),len(mat[0])
         for i in range(m):
             for j in range(n):
@@ -10,3 +34,4 @@ class Solution:
                     nx+=1
                     ny+=1
         return mat
+        '''
