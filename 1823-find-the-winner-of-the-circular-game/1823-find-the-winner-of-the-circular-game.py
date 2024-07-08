32575448f4ab4b2e66d542340f7f8f0ec1edb395
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-08 16:58:07 +0900

    Time: 150 ms (18.13%), Space: 16.5 MB (49.47%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,20 +1,23 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        temp={i:i+1 for i in range(1,n+1)}
-        temp[n]=1
+        temp={i:[] for i in range(1,n+1)}
+        for i in range(1,n+1):
+            left=i-1 if i-1>0 else n
+            right=i+1 if i+1<=n else 1
+            temp[i].append(left)
+            temp[i].append(right)
 
         start=1
-        while sum(1 for value in temp.values() if value!=0)>1:
-            cnt, previous=0 if k>1 else -1, 0
-            while cnt<k-1:
-                previous=start
-                start=temp[start]
+        while True:
+            cnt=0
+            while cnt<k:
+                start=temp[start][1]
                 cnt+=1
             
-            temp[previous]=temp[start]
-            temp[start]=0
-            start=temp[previous]
-        
-        for i in temp.keys():
-            if temp[i]!=0:
-                return i
+            if start==temp[start][1]:
+                return start
+            
+            prev=temp[start][0]
+            temp[temp[prev][0]][1]=start
+            temp[start][0]=temp[prev][0]
+            temp[prev][0]=temp[prev][1]=0
