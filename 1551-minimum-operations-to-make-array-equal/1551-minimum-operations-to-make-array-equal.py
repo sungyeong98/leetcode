594a3a0057d7fd4d18028ab92930e34b263c6c06
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-09 17:09:37 +0900

    Time: 35 ms (75.59%), Space: 16.6 MB (20.09%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,5 +1,6 @@
 class Solution:
     def minOperations(self, n: int) -> int:
+        #solution1
         '''
         num=n//2
         if n%2==1:  num+=1
@@ -12,8 +13,20 @@ class Solution:
         return result
         '''
 
+        #solution2
+        '''
         num = n//2 if n%2==0 else n//2+1
 
         target=(2*(num-1))+1 if n%2==1 else (2*(num-1))+2
 
         return sum([target-((2*i)+1) for i in range(num-1,-1,-1)])
+        '''
+
+        #solution3
+        total=n*(n-1)+n
+        mid=total//n
+
+        n=mid//2
+        if mid%2==1:
+            return n*(n+1)
+        return n*(n-1)+n
