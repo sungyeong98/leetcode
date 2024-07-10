144ffe21b63b219dda25ed07c5878c3fdeb8bf23
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-10 16:37:27 +0900

    Time: 53 ms (31.31%), Space: 16.7 MB (82.14%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,5 +1,7 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
+        #solution1
+        '''
         stack=[]
         for i in logs:
             if i!='../' and i!='./':
@@ -8,3 +10,13 @@ class Solution:
                 if len(stack)>0:
                     stack.pop(-1)
         return len(stack)
+        '''
+        result=0
+        for i in logs:
+            if i=='../':
+                result-=1 if result>0 else 0
+            elif i=='./':
+                pass
+            else:
+                result+=1
+        return result
