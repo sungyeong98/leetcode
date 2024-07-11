a46a69a3e14fdb6deb38374e0239104e0ad2d132
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-11 18:22:45 +0900

    Time: 31 ms (86.39%), Space: 16.7 MB (8.88%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -4,7 +4,8 @@ class Solution:
         result=0
         if week>0:
             result+=(28*week)+sum([7*i for i in range(week-1,-1,-1)])
-        
-        m=reduce(lambda x,y:x+y, list(range(week+1,week+day+1)))
+        m=0
+        if day>0:
+            m+=reduce(lambda x,y:x+y, list(range(week+1,week+day+1)))
 
         return result+m
