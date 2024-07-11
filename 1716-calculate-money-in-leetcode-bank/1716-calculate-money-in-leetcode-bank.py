5927aaf31a76690ec0cdcbf958527a6c4ad65a13
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-11 18:22:25 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,10 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        week,day=n//7, n%7
+        result=0
+        if week>0:
+            result+=(28*week)+sum([7*i for i in range(week-1,-1,-1)])
+        
+        m=reduce(lambda x,y:x+y, list(range(week+1,week+day+1)))
+
+        return result+m
