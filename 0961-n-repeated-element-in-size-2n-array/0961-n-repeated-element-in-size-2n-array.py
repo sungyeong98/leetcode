3b9370d1b2d8d0a23b24b2cac968f55f4528fc80
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-12 18:43:10 +0900

    Time: 165 ms (62.68%), Space: 17.9 MB (54.15%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -2,4 +2,4 @@ from collections import Counter
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
         n=Counter(nums)
-        return [i for i in n.keys() if n[i]==max(n.values())][0]
+        return n.most_common(1)[0][0]
