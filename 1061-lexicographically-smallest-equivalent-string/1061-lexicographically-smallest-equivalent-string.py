18381e5d6a664f09ff63cde741a771eb534fbdea
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-13 19:07:01 +0900

    Time: 71 ms (6%), Space: 16.7 MB (8.78%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,23 +1,38 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        word=[]
+        s=set(s1+s2+baseStr)
+        temp={i:float('inf') for i in s}
+        level=1
+
         for w1,w2 in zip(s1,s2):
-            if not word:
-                word.append(set({w1,w2}))
+            if temp[w1]!=float('inf') or temp[w2]!=float('inf'):
+                min_level=min(temp[w1],temp[w2])
+                temp[w1]=min_level
+                temp[w2]=min_level
             else:
-                for sub in word:
-                    if w1 in sub or w2 in sub:
-                        sub.add(w1)
-                        sub.add(w2)
-                        break
+                temp[w1]=level
+                temp[w2]=level
+                level+=1
+        
+        for _ in range(len(s)):
+            for w1,w2 in zip(s1,s2):
+                min_level=min(temp[w1],temp[w2])
+                temp[w1]=min_level
+                temp[w2]=min_level
+        
+        level_dict={}
+        for w in temp:
+            l=temp[w]
+            if l!=float('inf'):
+                if l not in level_dict:
+                    level_dict[l]=w
                 else:
-                    word.append(set({w1,w2}))
+                    level_dict[l]=min(level_dict[l],w)
+        
         result=''
-        for i in baseStr:
-            for sub in word:
-                if i in sub:
-                    result+=min(sub)
-                    break
+        for w in baseStr:
+            if temp[w]==float('inf'):
+                result+=w
             else:
-                result+=i
+                result+=level_dict[temp[w]]
         return result
