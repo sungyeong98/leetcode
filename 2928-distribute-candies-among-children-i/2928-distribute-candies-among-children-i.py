20541fa79d11b323327410492be060a425bdb9f7
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-15 18:22:46 +0900

    Time: 48 ms (81.09%), Space: 16.3 MB (95.09%) - LeetHub

--- 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.py
@@ -1,8 +1,20 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
+        #solution1
+        '''
         nums=[i for i in range(limit+1)]
         result=0
         for i in product(nums,repeat=3):
             if sum(list(i))==n:
                 result+=1
+        return result
+        '''
+
+        #solution2
+        result=0
+        for i in range(min(n,limit)+1):
+            for j in range(min(n-i,limit)+1):
+                num=n-i-j
+                if 0<=num<=limit:
+                    result+=1
         return result
