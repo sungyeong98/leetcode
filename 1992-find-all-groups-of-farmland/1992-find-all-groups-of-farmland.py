c3e4aa1fd01d737435c1da0cdf440468b4bf4bbd
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-15 18:56:07 +0900

    Time: 1024 ms (25.34%), Space: 41.2 MB (24.53%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -4,28 +4,29 @@ class Solution:
         m,n=len(land),len(land[0])
         result,visited=[],set()
 
-        for i in range(m):
-            for j in range(n):
-                if (i,j) not in visited and land[i][j]==1:
-                    stack=[(i,j)]
-                    visited.add((i,j))
-                    min_x,min_y=i,j
-                    max_x,max_y=i,j
+        target=[(i,j) for i in range(m) for j in range(n) if land[i][j]==1]
 
-                    while stack:
-                        x,y=stack.pop()
-                        if x<min_x:     min_x=x
-                        if x>max_x:     max_x=x
-                        if y<min_y:     min_y=y
-                        if y>max_y:     max_y=y
+        for i,j in target:
+            if (i,j) not in visited and land[i][j]==1:
+                stack=[(i,j)]
+                visited.add((i,j))
+                min_x,min_y=i,j
+                max_x,max_y=i,j
 
-                        for dx,dy in dir:
-                            nx=x+dx
-                            ny=y+dy
+                while stack:
+                    x,y=stack.pop()
+                    if x<min_x:     min_x=x
+                    if x>max_x:     max_x=x
+                    if y<min_y:     min_y=y
+                    if y>max_y:     max_y=y
 
-                            if 0<=nx<m and 0<=ny<n and (nx,ny) not in visited and land[nx][ny]==1:
-                                stack.append((nx,ny))
-                                visited.add((nx,ny))
-                    
-                    result.append([min_x,min_y,max_x,max_y])
+                    for dx,dy in dir:
+                        nx=x+dx
+                        ny=y+dy
+
+                        if 0<=nx<m and 0<=ny<n and (nx,ny) not in visited and land[nx][ny]==1:
+                            stack.append((nx,ny))
+                            visited.add((nx,ny))
+                
+                result.append([min_x,min_y,max_x,max_y])
         return result
