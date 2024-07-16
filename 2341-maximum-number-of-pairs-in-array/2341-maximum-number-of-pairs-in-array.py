f6c077f970660345ef766bd8cd361c082db94271
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-16 21:03:27 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfPairs(self, nums: List[int]) -> List[int]:
+        n=Counter(nums)
+
+        result=[0,0]
+        for i in n:
+            if n[i]==2:
+                result[0]+=1
+            elif n[i]>2:
+                result[0]+=n[i]//2
+                result[1]+=1
+            else:
+                result[1]+=1
+        return result
