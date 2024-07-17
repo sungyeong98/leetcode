cc039edd7d00be6fc21dee8b01f57855d395854b
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-17 20:33:58 +0900

    Time: 43 ms (35.82%), Space: 16.6 MB (28.15%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,6 +1,6 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        #solution1
+        #solution1(너무 느림)
         '''
         n=len(nums[0])
         temp=[]
@@ -13,10 +13,27 @@ class Solution:
         return result[0]
         '''
 
-        #solution2
+        #solution2(140ms)
+        '''
         n=len(nums[0])
         nums=[int(i,2) for i in nums]
         num=[i for i in range(2**n)]
         temp=list(set(num)-set(nums))
         result=bin(temp[-1])[2:]
         return '0'*(n-len(result))+result
+        '''
+
+        #solution3
+        if nums == ["1"]:
+            return "0"
+        n = len(nums)
+        s = set(nums)
+        largest = int('1'*n, 2)
+
+        for i in range(largest, 0, -1):
+            b = str(bin(i))[2:]
+            if len(b) != n:
+                b = '0'*(n-len(b)) + b
+
+            if b not in s:
+                return b
