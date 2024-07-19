edc6ce571ced84357d99d1c56c4f20d6abb9afc2
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-19 19:03:44 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,5 +1,7 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        #solution1
+        '''
         player={}
         for winner,loser in matches:
             if winner not in player:
@@ -15,3 +17,14 @@ class Solution:
         one_lose_players=[i for i in player.keys() if sum(player[i])>0 and player[i][1]==1]
 
         return [sorted(all_winners), sorted(one_lose_players)]
+        '''
+        undefeated, one_loss, two_loss = set(), set(), set()
+        for winner, loser in matches:
+            undefeated.add(winner)
+            
+            if loser in one_loss:
+                two_loss.add(loser)
+            else:
+                one_loss.add(loser)
+        no_loss_list = sorted(undefeated - one_loss)
+        one_loss_list = sorted(one_loss - two_loss)
