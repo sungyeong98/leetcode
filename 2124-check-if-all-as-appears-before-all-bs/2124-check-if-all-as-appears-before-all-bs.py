2bc8e26f6d47bac22afde89b7bb3bd10e722acab
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-23 19:13:54 +0900

    Time: 36 ms (45.6%), Space: 16.4 MB (90.75%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,7 +1,13 @@
 class Solution:
     def checkString(self, s: str) -> bool:
+        #solution1
+        '''
         a=list(filter(lambda x:s[x]=='a', range(len(s))))
         b=list(filter(lambda x:s[x]=='b', range(len(s))))
         if a and b and max(a)>min(b):
             return False
         return True
+        '''
+
+        #solution2
+        return sorted(s)==list(s)
