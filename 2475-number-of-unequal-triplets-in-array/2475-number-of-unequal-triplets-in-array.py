32f0beddb32bc2903e9bb0ff1ba77afcdfc91ba4
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-23 19:46:04 +0900

    Time: 40 ms (91.24%), Space: 16.7 MB (9.73%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.py
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def unequalTriplets(self, nums: List[int]) -> int:
+        #solution1(216ms)
+        '''
         result,n=0,len(nums)
         for i in range(n-2):
             for j in range(i+1,n-1):
@@ -9,3 +11,18 @@ class Solution:
                     if nums[i]!=nums[k] and nums[j]!=nums[k]:
                         result+=1
         return result
+        '''
+
+        #solution2
+        c = Counter(nums)
+        res = 0
+        
+        left = 0
+        right = len(nums)
+        
+        for freq in c.values():
+            right -= freq
+            res += left * freq * right
+            left += freq
+        
+        return res
