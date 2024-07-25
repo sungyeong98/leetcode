09830b1fe448032d10ad6166ac6a21940f7336f1
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-25 17:19:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        aa=bin(a)[2:][::-1]
+        bb=bin(b)[2:][::-1]
+        cc=bin(c)[2:][::-1]
+        result=0
+        for n1,n2,n3 in zip_longest(aa,bb,cc,fillvalue='0'):
+            if n3=='1':
+                if n1=='0' and n2=='0':
+                    result+=1
+            else:
+                if n1=='1' and n2=='1':
+                    result+=2
+        return result
