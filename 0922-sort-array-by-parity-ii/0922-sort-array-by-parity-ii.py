8bf3605c58d5672d3215f97d69fbca2b9fa3f7a1
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-26 16:54:49 +0900

    Time: 153 ms (43.86%), Space: 19.2 MB (25.83%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.py
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        for i in range(n-1):
-            for j in range(i+1,n):
-                if (i%2==0 and nums[i]%2==1 and j%2==1 and nums[j]%2==0)\
-                    or (i%2==1 and nums[i]%2==0 and j%2==0 and nums[j]%2==1):
-                    nums[i],nums[j]=nums[j],nums[i]
-        return nums
+        even=[i for i in nums if i%2==0]
+        odd=[i for i in nums if i%2==1]
+        result=[]
+        for e,o in zip(even, odd):
+            result.append(e)
+            result.append(o)
+        return result
