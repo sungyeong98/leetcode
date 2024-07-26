9584914b5a9073c5771bef14027df2b5b98e1c1b
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-26 17:37:20 +0900

    Time: 45 ms (53.37%), Space: 16.5 MB (89.26%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,22 +1,8 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        piles=deque(piles)
-        alice=[]
-        bob=[]
-
-        while piles:
-            if piles[-1]>piles[0]:
-                alice.append(piles.pop())
-            elif piles[0]>piles[-1]:
-                alice.append(piles.popleft())
-            else:
-                alice.append(piles.pop())
-
-            if piles[-1]>piles[0]:
-                bob.append(piles.pop())
-            elif piles[0]>piles[-1]:
-                bob.append(piles.popleft())
-            else:
-                bob.append(piles.pop())
-        
-        return sum(alice)>sum(bob)
+        n=sorted(piles)
+        alice,bob=0,0
+        while n:
+            alice+=n.pop()
+            bob+=n.pop()
+        return alice>bob
