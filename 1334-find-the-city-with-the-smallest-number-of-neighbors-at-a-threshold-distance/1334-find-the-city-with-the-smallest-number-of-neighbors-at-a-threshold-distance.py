cd781357d4388e52dc1c336e99ef673af8b36ddf
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-26 15:46:05 +0900

    Time: 161 ms (97.95%), Space: 17.9 MB (40.9%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -2,30 +2,30 @@ class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         graph={i:[] for i in range(n)}
         for s,e,w in edges:
-            graph[s].append((e,w))
-            graph[e].append((s,w))
+            if w<=distanceThreshold:
+                graph[s].append((e,w))
+                graph[e].append((s,w))  
 
-        result={}
-        for node in graph:
-            temp=set()
-            if result and len(graph[node])>min(result.values()):
-                continue
+        def find(start):
+            min_heap=[(0,start)]
+            dist={i:float('inf') for i in range(n)}
+            dist[start]=0
 
-            for s_node,s_wei in graph[node]:
-                if s_wei>distanceThreshold:
+            while min_heap:
+                d, u = heappop(min_heap)
+                if d>dist[u]:
                     continue
-                temp.add(s_node)
+                for v,w in graph[u]:
+                    if dist[u]+w<dist[v]:
+                        dist[v]=dist[u]+w
+                        heappush(min_heap, (dist[v],v))
+            return dist
 
-                stack=[(s_node,s_wei)]
-
-                while stack:
-                    cur_node,cur_wei=stack.pop()
-
-                    for next_node, next_wei in graph[cur_node]:
-                        if next_node!=node and next_node not in temp and cur_wei+next_wei<=distanceThreshold:
-                            stack.append((next_node,next_wei+cur_wei))
-                            temp.add(next_node)
-
-            result[node]=len(temp)
-        result=sorted(result.items(), key=lambda x:(x[1],-x[0]))
-        return result[0][0]
+        min_num, result=n,-1
+        for start in range(n):
+            dis=find(start)
+            cnt=sum([1 for d in dis.values() if d<=distanceThreshold])
+            if cnt<min_num or (cnt==min_num and start>result):
+                min_num=cnt
+                result=start
+        return result
