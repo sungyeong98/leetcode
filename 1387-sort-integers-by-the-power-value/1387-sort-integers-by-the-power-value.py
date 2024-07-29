b8622f8d383396664e37d517773c13b25570dd7e
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-29 20:08:40 +0900

    Time: 152 ms (67.18%), Space: 16.6 MB (74.24%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,5 +1,7 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
+        #solution1(6132ms   ->  효율성이 너무 떨어짐)
+        '''
         cnt={i:0 for i in range(lo,hi+1)}
         num=[i for i in range(lo,hi+1)]
         visited=set()
@@ -24,3 +26,30 @@ class Solution:
 
         result=sorted(cnt.keys(), key=lambda x:(cnt[x],x))
         return result[k-1]
+        '''
+
+        #solution2
+        def power(n):
+            if n in memo:
+                return memo[n]
+
+            cnt=0
+            ori_n=n
+
+            while n!=1:
+                if n in memo:
+                    cnt+=memo[n]
+                    break
+                if n%2==0:
+                    n//=2
+                else:
+                    n=3*n+1
+                cnt+=1
+            memo[ori_n]=cnt
+            return cnt
+        memo={}
+        result=[]
+        for num in range(lo,hi+1):
+            result.append((power(num),num))
+        result.sort()
+        return result[k-1][1]
