b203df8843acf4069a99f9076132528473b4b433
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-29 17:15:41 +0900

    Time: 580 ms (39.24%), Space: 16.6 MB (66.67%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -1,8 +1,23 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
         result=0
-        for i in combinations(rating, 3):
-            temp=list(i)
-            if temp[0]>temp[1]>temp[2] or temp[0]<temp[1]<temp[2]:
-                result+=1
+        n=len(rating)
+
+        for i in range(n):
+            left_s,left_l=0,0
+            right_s,right_l=0,0
+
+            for j in range(i):
+                if rating[j]<rating[i]:
+                    left_s+=1
+                if rating[j]>rating[i]:
+                    left_l+=1
+                
+            for k in range(i+1,n):
+                if rating[k]<rating[i]:
+                    right_s+=1
+                if rating[k]>rating[i]:
+                    right_l+=1
+            
+            result+=left_s*right_l+left_l*right_s
         return result
