c7da51841285bd01a1a1eff950b9107fcc9d4701
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-30 17:13:23 +0900

    Time: 354 ms (90.54%), Space: 22.4 MB (17.27%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,5 +1,7 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
+        #solution1
+        '''
         avg=sum(weights)//days
         result=0
 
@@ -22,3 +24,24 @@ class Solution:
             else:
                 result=avg
                 return result
+        '''
+        def isValid(cap):
+            day,total=1,0
+            for w in weights:
+                total+=w
+                if total>cap:
+                    total=w
+                    day+=1
+                    if day>days:
+                        return False
+            return True
+
+        left,right=max(weights),sum(weights)
+        while left<right:
+            mid=(left+right)//2
+
+            if isValid(mid):
+                right=mid
+            else:
+                left=mid+1
+        return left
