b29e9ecbe98d646ee5448d9527f617205b16f475
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-07-30 16:59:24 +0900

    Time: 427 ms (41.33%), Space: 17.9 MB (40%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,11 +1,12 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        if len(s)==1:   return 0
-        if 'a' not in s or 'b' not in s:    return 0
-        result=int(1e9)
-        for i in range(len(s)+1):
-            a_cnt=s[:i].count('b')
-            b_cnt=s[i:].count('a')
+        left,right=0,s.count('a')
+        result=left+right
 
-            result=min(result,a_cnt+b_cnt)
+        for i in range(len(s)):
+            if s[i]=='a':
+                right-=1
+            else:
+                left+=1
+            result=min(result,left+right)
         return result
