940e709e6f524c4cc758bc648dc7dd178ca30f18
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-01 19:10:08 +0900

    Time: 56 ms (50.83%), Space: 17.1 MB (34.16%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,4 +1,22 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        if n==1:    return [['Q']]
-        if n==2 or n==3:    return [['']]
+        result=[]
+        def isvalid(board,row,col):
+            for r,c in enumerate(board):
+                if col==c or abs(row-r)==abs(col-c):
+                    return False
+            return True
+        def backtrack(level,board,visited):
+            if level==n:
+                result.append(['.'*i+'Q'+'.'*(n-i-1) for i in board])
+                return
+            
+            for i in range(n):
+                if i not in visited and isvalid(board,level,i):
+                    board.append(i)
+                    visited.add(i)
+                    backtrack(level+1, board, visited)
+                    board.pop()
+                    visited.remove(i)
+        backtrack(0,[],set())
+        return result
