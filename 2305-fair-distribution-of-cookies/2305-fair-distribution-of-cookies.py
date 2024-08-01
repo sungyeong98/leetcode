ca56e4c323598317211ad306b3d98e1352276ff6
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-01 19:34:43 +0900

    Time: 352 ms (56.49%), Space: 16.5 MB (36.05%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,15 +1,19 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-        result=[]
-        def backtrack(children,c):
+        result=int(1e9)
+        def backtrack(children,c,cur_max):
+            nonlocal result
             if not c:
-                result.append(max(children))
+                result=min(result,cur_max)
                 return
             num=c.pop()
             for i in range(k):
                 children[i]+=num
-                backtrack(children,c)
+                next_max=max(cur_max, children[i])
+                if next_max<result:
+                    backtrack(children,c,next_max)
                 children[i]-=num
             c.append(num)
-        backtrack([0]*k,cookies)
-        return min(result)
+
+        backtrack([0]*k,cookies,0)
+        return result
