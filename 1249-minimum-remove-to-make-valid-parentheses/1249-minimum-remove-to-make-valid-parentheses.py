cb531ac11494209a8838270f0c2c8a11e69cbc30
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-05 16:27:08 +0900

    Time: 65 ms (87.73%), Space: 18.2 MB (65.62%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,5 +1,7 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        #solution1
+        '''
         left=[idx for idx,val in enumerate(s) if val=='(']
         right=[idx for idx,val in enumerate(s) if val==')']
         l=r=0
@@ -18,3 +20,20 @@ class Solution:
                 result+=val
 
         return result
+        '''
+
+        #solution2
+        stack=[]
+        s_list=list(s)
+
+        for idx,val in enumerate(s_list):
+            if val=='(':
+                stack.append(idx)
+            elif val==')':
+                if stack:
+                    stack.pop()
+                else:
+                    s_list[idx]=''
+        while stack:
+            s_list[stack.pop()]=''
+        return ''.join(s_list)
