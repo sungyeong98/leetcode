2b17cfe453a238ee2611164d3d86160b324082e7
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-05 20:41:01 +0900

    Time: 39 ms (44%), Space: 16.6 MB (19.74%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
@@ -3,7 +3,7 @@ class Solution:
         counter=Counter(s)
 
         num=[i for i in counter.keys() if counter[i]>1]
-        result=0
+        result=-1
         for n in num:
             idx=[idx for idx,val in enumerate(s) if val==n]
             result=max(idx[-1]-idx[0]-1,result)
