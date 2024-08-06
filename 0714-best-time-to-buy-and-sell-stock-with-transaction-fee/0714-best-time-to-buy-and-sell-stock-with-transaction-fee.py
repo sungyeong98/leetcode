8a5fe4077c5a004e2885271ae420459850e9de6c
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-06 17:58:51 +0900

    Time: 528 ms (60.12%), Space: 23.7 MB (48.94%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,21 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n=len(prices)
-        result=0
-        def backtrack(idx,val,flag):
-            nonlocal result
-            if idx==n-1:
-                if val>0:
-                    result=max(result,val)
-                return
-            
-            if flag:
-                for i in range(idx+1,n):
-                    cnt=(prices[i]-prices[idx]-fee)
-                    if cnt>0:
-                        backtrack(i,val+cnt,False)
-            else:
-                for i in range(idx,n):
-                    backtrack(i,val,True)
-        backtrack(0,0,False)
-        return result
+
+        no_stock=[0]*n
+        has_stock=[0]*n
+
+        has_stock[0]=-prices[0]
+
+        for i in range(1,n):
+            no_stock[i]=max(no_stock[i-1], has_stock[i-1]+prices[i]-fee)
+            has_stock[i]=max(has_stock[i-1], no_stock[i-1]-prices[i])
+        
+        return no_stock[-1]
