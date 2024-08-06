48ba3124813f4057ad650673e4abeb35f596ff27
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-06 19:18:28 +0900

    Time: 113 ms (62.58%), Space: 17.3 MB (64.76%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
@@ -1,7 +1,27 @@
 class Solution:
     def numSplits(self, s: str) -> int:
+        left_count=defaultdict(int)
+        right_count=defaultdict(int)
+
+        for i in s:
+            right_count[i]+=1
+
+        distinct_left=0
+        distinct_right=len(right_count)
+
         result=0
-        for i in range(1,len(s)):
-            if len(set(s[:i]))==len(set(s[i:])):
+
+        for i in range(len(s)-1):
+            word=s[i]
+
+            left_count[word]+=1
+            if left_count[word]==1:
+                distinct_left+=1
+            
+            right_count[word]-=1
+            if right_count[word]==0:
+                distinct_right-=1
+            
+            if distinct_left==distinct_right:
                 result+=1
         return result
