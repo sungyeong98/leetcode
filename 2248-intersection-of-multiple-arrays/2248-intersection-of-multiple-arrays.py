a1f9ff7ff88be96b88b375eb9c5753356d527ee9
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-06 19:48:05 +0900

    Time: 67 ms (45.04%), Space: 17.1 MB (9.75%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -1,3 +1,3 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        return reduce(lambda x,y:set(x)&set(y),nums)
+        return sorted(reduce(lambda x,y:set(x)&set(y),nums))
