3ac0a7f16eb9b60c83aa1c5d897c5eba877162af
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-08 19:01:04 +0900

    Time: 100 ms (95.78%), Space: 16.9 MB (42.69%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,30 +1,19 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        if len(set(s))<3:
-            return 0
-        word_idx=defaultdict(list)
-        n=len(s)
         result=0
+        n=len(s)
 
-        for idx,w in enumerate(s):
-            word_idx[w].append(idx)
+        a=b=c=-1
 
-        for word in word_idx:
-            for idx in word_idx[word]:
-                a_list=[i for i in word_idx['a'] if i>idx]
-                b_list=[i for i in word_idx['b'] if i>idx]
-                c_list=[i for i in word_idx['c'] if i>idx]
-
-                if word=='a':
-                    if b_list and c_list:
-                        temp=max(min(b_list),min(c_list))
-                        result+=n-temp
-                elif word=='b':
-                    if a_list and c_list:
-                        temp=max(min(a_list),min(c_list))
-                        result+=n-temp
-                else:
-                    if a_list and b_list:
-                        temp=max(min(a_list),min(b_list))
-                        result+=n-temp
+        for i in range(n):
+            if s[i]=='a':
+                a=i
+            elif s[i]=='b':
+                b=i
+            else:
+                c=i
+            
+            if a!=-1 and b!=-1 and c!=-1:
+                temp=min(a,b,c)
+                result+=temp+1
         return result
