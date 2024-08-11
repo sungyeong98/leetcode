09cc0c4084287cdbd72794a4a2897efdd7d5e4a2
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-11 18:14:51 +0900

    Time: 208 ms (16.8%), Space: 17 MB (93.39%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.py
+++ 1447-simplified-fractions/1447-simplified-fractions.py
@@ -1,10 +1,17 @@
 class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
+        if n==1:    return []
+
         result=[]
-        if n==1:    return result
         for i in range(2,n+1):
             temp='/'+str(i)
             for j in range(1,i):
-                if (i%j)%2==1 or j==1:
+                flag=True
+                if j>1:
+                    for k in range(1,j+1):
+                        if k!=1 and (j%k==0 and i%k==0):
+                            flag=False
+                            break
+                if flag:
                     result.append(str(j)+temp)
         return result
