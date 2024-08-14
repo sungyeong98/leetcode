9aa320af7495a7eca12de6530f103d1c7260bbad
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-14 16:49:52 +0900

    Time: 83 ms (88.54%), Space: 17.5 MB (67.27%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.py
@@ -1,21 +1,21 @@
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        n=len(nums)
-        counter=Counter(nums)
-        cnt=defaultdict(int)
+        nums.sort()
+        left,right=0,nums[-1]-nums[0]
 
-        for i in range(n-1):
-            num=nums[i]
-            counter[num]-=1
-            if counter[num]==0:
-                counter.pop(num)
-            for j in counter:
-                cnt[abs(num-j)]+=counter[j]
-        cnt=dict(sorted(cnt.items(), key=lambda x:x[0]))
+        def count(nums, dis):
+            cnt,left=0,0
+            for right in range(1,len(nums)):
+                while nums[right]-nums[left]>dis:
+                    left+=1
+                cnt+=right-left
+            return cnt
 
-        result=0
-        for i in cnt:
-            if cnt[i]<k:
-                k-=cnt[i]
+        while left<right:
+            mid=(left+right)//2
+            if count(nums,mid)<k:
+                left=mid+1
             else:
-                return i
+                right=mid
+        
+        return left
