82f03d44c2c00bb1a76b38776ce099b75574da9b
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-16 18:31:58 +0900

    Time: 20 ms (100%), Space: 16.6 MB (38.19%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.py
+++ 0899-orderly-queue/0899-orderly-queue.py
@@ -1,15 +1,10 @@
 class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:
-        s=list(s)
-        result=[]
-
-        while k>0:
-            target_word=min(s)
-            target_idx=s.index(target_word)
-
-            result.append(target_word)
-            s=s[target_idx+1:]+s[:target_idx]
-            k-=1
-
-        result+=s
-        return ''.join(result)
+        if k>1:
+            return ''.join(sorted(s))
+        
+        result=s
+        for i in range(len(s)):
+            s=s[1:]+s[0]
+            result=min(result,s)
+        return result
