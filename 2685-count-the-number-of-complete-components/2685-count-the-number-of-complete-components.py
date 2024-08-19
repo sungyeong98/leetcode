8e080d7da2271d2edff3331a916db233dfcea2bb
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-19 14:31:29 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph=defaultdict(list)
+        for x,y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        result=0
+        visited=set()
+        for i in range(n):
+            if i not in visited:
+                if i not in graph:
+                    visited.add(i)
+                    result+=1
+                    continue
+
+                temp, stack = set(), [i]
+                temp.add(i)
+                while stack:
+                    node=stack.pop()
+                    for next_node in graph[node]:
+                        if next_node not in temp:
+                            temp.add(next_node)
+                            stack.append(next_node)
+                cnt=len(set([len(graph[i]) for i in temp]))
+                if cnt==1:
+                    result+=1
+                
+                visited.update(temp)
+        return result
