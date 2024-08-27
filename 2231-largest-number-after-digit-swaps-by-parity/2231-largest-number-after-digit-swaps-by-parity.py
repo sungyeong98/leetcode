7b310a8ad659ba2a6281ff846320d3474f52271a
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-27 16:13:06 +0900

    Time: 31 ms (78.79%), Space: 16.5 MB (89.47%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.py
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.py
@@ -1,14 +1,12 @@
 class Solution:
     def largestInteger(self, num: int) -> int:
         n=list(str(num))
-        even=sorted([i for i in n if int(i)%2==0],reverse=True)
-        odd=sorted([i for i in n if int(i)%2==1],reverse=True)
-        if int(n[0])%2==1:
-            even,odd=odd,even
+        even=iter(sorted([i for i in n if int(i)%2==0],reverse=True))
+        odd=iter(sorted([i for i in n if int(i)%2==1],reverse=True))
         result=''
-        for e,o in zip_longest(even,odd,fillvalue=None):
-            if e!=None:
-                result+=e
-            if o!=None:
-                result+=o
+        for i in n:
+            if int(i)%2==0:
+                result+=next(even)
+            else:
+                result+=next(odd)
         return int(result)
