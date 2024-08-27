c87fbae84ef31a9ceda0ac70b32e6e7be84cbe8b
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-27 13:24:19 +0900

    Time: 583 ms (23.68%), Space: 29.5 MB (31.06%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,24 +1,16 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
         graph=defaultdict(list)
-        for (x,y),p in zip(edges,succProb):
-            graph[x].append((y,p))
-            graph[y].append((x,p))
-
-        result=0
-        visited=set()
-        visited.add(start)
-
-        def backtrack(cur_node,cur_prob,visited):
-            nonlocal result
-            if cur_node==end:
-                result=max(result,cur_prob)
-                return
-            
-            for next_node, next_prob in graph[cur_node]:
-                if next_node not in visited:
-                    visited.add(next_node)
-                    backtrack(next_node,cur_prob*next_prob if cur_prob!=0 else next_prob, visited)
-                    visited.remove(next_node)
-        backtrack(start,0,visited)
-        return result
+        for idx,(x,y) in enumerate(edges):
+            graph[x].append((y,idx))
+            graph[y].append((x,idx))
+        q=deque([start])
+        prob=[0.0]*n
+        prob[start]=1.0
+        while q:
+            cur_node=q.popleft()
+            for next_node, idx in graph[cur_node]:
+                if prob[cur_node]*succProb[idx]>prob[next_node]:
+                    prob[next_node]=prob[cur_node]*succProb[idx]
+                    q.append(next_node)
+        return prob[end]
