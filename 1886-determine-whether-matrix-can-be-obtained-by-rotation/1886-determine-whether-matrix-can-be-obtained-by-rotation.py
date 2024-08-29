bc4b53db78204124ec83ec3df9f1dd346fdbe347
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-29 13:04:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        n=len(mat)
+        def rotate_90(mat):
+            temp=[[0]*n for _ in range(n)]
+
+            for i in range(n):
+                for j in range(n):
+                    temp[j][n-1-i]=mat[i][j]
+            return temp
+        def rotate_180(mat):
+            temp=[[0]*n for _ in range(n)]
+
+            for i in range(n):
+                for j in range(n):
+                    temp[n-1-i][n-1-j]=mat[i][j]
+            return temp
+        def rotate_270(mat):
+            temp=[[0]*n for _ in range(n)]
+
+            for i in range(n):
+                for j in range(n):
+                    temp[n-1-j][i]=mat[i][j]
+            return temp
+        
+        t1,t2,t3=rotate_90(mat),rotate_180(mat),rotate_270(mat)
+        return True if target==t1 or target==t2 or target==t3 else False
