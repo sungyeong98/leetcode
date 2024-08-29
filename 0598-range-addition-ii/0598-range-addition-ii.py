4c3441f8fa0449dbbaa81eb6b913427fbc66019b
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-29 13:50:49 +0900

    Time: 67 ms (57.84%), Space: 18.6 MB (5.47%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.py
+++ 0598-range-addition-ii/0598-range-addition-ii.py
@@ -1,22 +1,7 @@
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        if not ops:
-            return m*n
-        board=[[0]*(n+1) for _ in range(m+1)]
-        for x,y in ops:
-            board[0][0]+=1
-            board[x][y]+=1
-            board[0][y]-=1
-            board[x][0]-=1
-
-        for j in range(n):
-            for i in range(1,m):
-                board[i][j]+=board[i-1][j]
-
-        for i in range(m):
-            for j in range(1,n):
-                board[i][j]+=board[i][j-1]
-        
-        temp=[board[i][j] for i in range(m) for j in range(n)]
-        c=Counter(temp)
-        return c[max(c.keys())]
+        min_r, min_c = m, n
+        for i in range(len(ops)):
+            min_r=min(min_r, ops[i][0])
+            min_c=min(min_c, ops[i][1])
+        return min_r*min_c
