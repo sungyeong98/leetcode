d6da0334e601a2a5be320ba8276d5adf222ec53a
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-29 12:51:28 +0900

    Time: 85 ms (79.34%), Space: 16.6 MB (78.88%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
+        '''
         nums.sort()
         n=len(nums)
         result=float('inf')
@@ -7,4 +8,17 @@ class Solution:
             high=nums[i]
             for j in range(i-k+1,-1,-1):
                 result=min(result,high-nums[j])
+        return result
+        '''
+
+        n=len(nums)
+        if k>n:
+            return -1
+        nums.sort()
+        result=float('inf')
+        l=0
+        for i in range(k-1, n):
+            diff=nums[i]-nums[l]
+            result=min(result,diff)
+            l+=1
         return result
