de8403f4012720e087d0bad2139cc6ed5416eac0
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-08-30 15:18:02 +0900

    Time: 34 ms (70.91%), Space: 16.6 MB (48.29%) - LeetHub

--- 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.py
+++ 3222-find-the-winning-player-in-coin-game/3222-find-the-winning-player-in-coin-game.py
@@ -1,4 +1,4 @@
 class Solution:
     def losingPlayer(self, x: int, y: int) -> str:
-        if y<4: return 'Bob'
-        return 'Alice' if min(x//2,y//8)%2==0 else 'Bob'
+        temp=min(x,y//4)
+        return 'Alice' if temp%2 else 'Bob'
