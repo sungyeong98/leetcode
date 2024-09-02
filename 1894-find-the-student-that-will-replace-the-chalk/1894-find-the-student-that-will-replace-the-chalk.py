cf8320842a1cd4189536515a8bb01a7c62969dd8
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-02 13:41:50 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -0,0 +1,11 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        n=len(chalk)
+        idx=0
+        while k>0:
+            if chalk[idx]>k:
+                return idx
+            
+            k-=chalk[idx]
+            idx=(idx+1)%n
+        return idx
