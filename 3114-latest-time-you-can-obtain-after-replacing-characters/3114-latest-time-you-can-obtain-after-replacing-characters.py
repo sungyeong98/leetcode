1caf77ae81f71bd79f8d5d21162139970461228f
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-05 13:31:16 +0900

    Time: 42 ms (23.23%), Space: 16.4 MB (74.95%) - LeetHub

--- 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.py
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.py
@@ -8,17 +8,21 @@ class Solution:
                 fixed_hour='1'+hour[1]
             else:
                 fixed_hour='0'+hour[1]
-        else:
+        elif hour[1]=='?':
             if hour[0]=='0':
                 fixed_hour=hour[0]+'9'
             else:
                 fixed_hour=hour[0]+'1'
+        else:
+            fixed_hour=hour
         
         if minute=='??':
             fixed_minute='59'
         elif minute[0]=='?':
             fixed_minute='5'+minute[1]
-        else:
+        elif minute[1]=='?':
             fixed_minute=minute[0]+'9'
+        else:
+            fixed_minute=minute
         
         return fixed_hour+':'+fixed_minute
