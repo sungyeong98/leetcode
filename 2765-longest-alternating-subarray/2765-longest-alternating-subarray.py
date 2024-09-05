18067650c7136726a4ebb53835363145c3f1fd80
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-05 13:59:39 +0900

    Time: 72 ms (82.97%), Space: 16.5 MB (57.73%) - LeetHub

--- 2765-longest-alternating-subarray/2765-longest-alternating-subarray.py
+++ 2765-longest-alternating-subarray/2765-longest-alternating-subarray.py
@@ -1,13 +1,11 @@
 class Solution:
     def alternatingSubarray(self, nums: List[int]) -> int:
         n=len(nums)
-        temp=[nums[i+1]-nums[i] for i in range(n-1)]+[0]
-        result=-1
-        stack=[]
-        for i in temp:
-            if (not stack and abs(i)==1) or stack[-1]==-i:
-                stack.append(i)
+        result,dp=-1,-1
+        for i in range(1,n):
+            if dp>0 and nums[i]==nums[i-2]:
+                dp+=1
             else:
-                result=max(result,len(stack)+1)
-                stack=[i]
+                dp=2 if nums[i]==nums[i-1]+1 else -1
+            result=max(result,dp)
         return result
