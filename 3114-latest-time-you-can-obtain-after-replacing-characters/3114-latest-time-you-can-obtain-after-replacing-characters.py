12918f68b92488c028151f1ee70d4475d630ff80
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-05 13:31:05 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findLatestTime(self, s: str) -> str:
+        hour,minute=s.split(':')
+        if hour=='??':
+            fixed_hour='11'
+        elif hour[0]=='?':
+            if hour[1] in '01':
+                fixed_hour='1'+hour[1]
+            else:
+                fixed_hour='0'+hour[1]
+        else:
+            if hour[0]=='0':
+                fixed_hour=hour[0]+'9'
+            else:
+                fixed_hour=hour[0]+'1'
+        
+        if minute=='??':
+            fixed_minute='59'
+        elif minute[0]=='?':
+            fixed_minute='5'+minute[1]
+        else:
+            fixed_minute=minute[0]+'9'
+        
+        return fixed_hour+':'+fixed_minute
