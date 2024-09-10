c1100dd23ba51fd49f2f6423542e98cbf6305f43
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-10 14:29:51 +0900

    Time: 36 ms (50.14%), Space: 16.6 MB (10.8%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.py
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.py
@@ -2,7 +2,7 @@ class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
         c1,v1=coordinate1[0],coordinate1[1]
         c2,v2=coordinate2[0],coordinate2[1]
-        if (c1 in 'aceg' and c2 in 'aceg') or (c1 in 'befh' and c2 in 'befh'):
+        if (c1 in 'aceg' and c2 in 'aceg') or (c1 in 'bdfh' and c2 in 'bdfh'):
             if int(v1)%2==int(v2)%2:
                 return True
             else:
