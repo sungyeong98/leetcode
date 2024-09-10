7c22ef1bd15042f8cb6ca298592f848133030e79
sungyeong98 <149341161+sungyeong98@users.noreply.github.com>
2024-09-10 14:25:01 +0900

    Time: 46 ms (6.21%), Space: 16.5 MB (51.89%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -11,14 +11,18 @@ class Solution:
             if not node.left and not node.right:
                 temp1.append(node.val)
                 return
-            chk1(node.left)
-            chk1(node.right)
+            if node.left:
+                chk1(node.left)
+            if node.right:
+                chk1(node.right)
         def chk2(node):
             if not node.left and not node.right:
                 temp2.append(node.val)
                 return
-            chk2(node.left)
-            chk2(node.right)
+            if node.left:
+                chk2(node.left)
+            if node.right:
+                chk2(node.right)
         chk1(root1)
         chk2(root2)
         return temp1==temp2
